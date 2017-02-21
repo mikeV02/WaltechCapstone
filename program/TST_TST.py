9c3479f3cc2801e163c4f1353cfaa8e5ac8959f7
Miguel <admin@mikedlv.com>
2017-02-21 16:06:34 -0600

    Auto stash for cherrypick of "python class for serial communication with arduino"

---
+++ program/TST_TST.py
@@ -0,0 +1,12 @@
+import serial
+import sys
+import time
+import tester
+import datetime
+import time
+import subprocess
+
+from subprocess import PIPE
+from tester import currentHW
+
+print currentHW
