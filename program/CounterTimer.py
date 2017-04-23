291b40df31e1bff2307d1f6006a785e745f7de30
cacartag <cacartag@email.uark.edu>
2017-04-23 16:18:12 -0500

    added internal class

--- program/CounterTimer.py
+++ program/CounterTimer.py
@@ -3,7 +3,7 @@
 class Counter():
     def __init__(self, name):
         self.name = name
-        self.prevInput = 45 
+        self.prevInput = 45
         self.currentValue = 0
         self.preset = 0
         self.done = 0
@@ -36,5 +36,18 @@ class Timer():
         self.Prevx = x
         self.Prevy = y
 		
-		
+class Internal():
+    def __init__(self, name):
+        self.name = name
+        self.done = 0
+        
+    def setLocationType(self, x, y,type):
+        self.x = x
+        self.y = y
+        self.type = type
+        
+    def setDoneBit(self, x, y, name):
+        self.Prevname = name
+        self.Prevx = x
+        self.Prevy = y
 		
